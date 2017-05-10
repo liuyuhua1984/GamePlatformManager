d897e42d4f1193c2d8d8d67f3fce663374805e85
liuyuhua <lyh@163.com>
2017-05-10 19:27:13 +0800

    跑通了beetl

---
+++ src/com/lyh/game/control/IndexController.java
@@ -0,0 +1,31 @@
+
+package com.lyh.game.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/** 
+ * ClassName:IndexController <br/> 
+ * Function: TODO (). <br/> 
+ * Reason:   TODO (). <br/> 
+ * Date:     2017年5月10日 下午3:38:23 <br/> 
+ * @author   lyh 
+ * @version   
+ * @see       
+ */
+@Controller
+public class IndexController {
+	
+	/** 
+	 * index:(). <br/> 
+	 * TODO().<br/> 
+	 * 
+	 * @author lyh 
+	 * @return 
+	 */  
+	@RequestMapping(value="/index")
+	public  String index(){
+		return "index";
+	}
+}
+  
